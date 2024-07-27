7f507bc5615935e991225dd0f92fbec09d1bf3a7
prabaharan_saminathan_2003 <139528188+prabaharan-saminathan@users.noreply.github.com>
2024-07-27 10:29:16 +0530

    Create com_2.java

---
+++ com_2.java
@@ -0,0 +1,15 @@
+public class com_2 {
+
+	public static void main(String[] args) {
+     int []a= {10,20,30,40};
+     int max=a[0];
+     for(int i=0;i<a.length;i++) {
+    	 if(a[i]>max) {
+    		 max=a[i];
+    	 }
+     }
+     System.out.println(max);
+     
+	}
+
+}
